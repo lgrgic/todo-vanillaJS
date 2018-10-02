297b4015dd61fc73f83390d14a64c5df957edde2
lgrgic <lukagrgic14@gmail.com>
2018-10-02 15:56:37 +0200

    new objects, toggle

--- version2.js
+++ version2.js
@@ -1,24 +1,42 @@
-let todos = ['item 1', 'item 2', 'item 3'];
-
-//It should have a function to dispaly todos.
-function displayTodos() {
-  console.log('My todos:', todos);
-}
-
-//It should have a function to add todos.
-function addTodo(todo) {
-  todos.push(todo);
-  displayTodos();
-}
-
-//It should have a function to change todos.
-function changeTodo(position, newValue) {
-  todos[position] = newValue;
-  displayTodos();
-}
-
-//It should have a function to delete todos.
-function deleteTodo(position) {
-  todos.splice(position, 1);
-  displayTodos();
-}
+let todoList = {
+  todos: [],
+  displayTodos: function() {
+    if (this.todos.length === 0) {
+      console.log('Your todo list is empty!');
+    } else {
+      console.log('My Todos:');
+      for (let i =0; i < this.todos.length; i++) {
+        if (this.todos[i].completed === true) {
+        console.log('(x)', this.todos[i].todoText);
+        } else {
+          console.log('()', this.todos[i].todoText)
+        }
+      }
+    }
+  },
+  //Create new item
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  //Change existing item
+  changeTodo: function(position, todoText) {
+    //this.todos[position] = newValue;
+    this.todos[position].todoText = todoText;
+    this.displayTodos();
+  },
+  //Delete existing item
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  //Mark as completed
+  toggleCompleted: function(position) {
+    let todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  }
+};
